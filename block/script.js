02a155c02a1901e4e1ebb9fd0cda0ed7f9e7dd21
박현우 <46562466+HyunsDev@users.noreply.github.com>
2020-08-30 02:19:49 +0900

    🎉 노션툴즈 이미지링크 배포! 1.0.0

--- block/script.js
+++ block/script.js
@@ -1,27 +1,29 @@
 function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+  return results === null
+    ? ""
+    : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 var img = getParameterByName("img");
 var link = getParameterByName("link");
 var theme = getParameterByName("theme");
-img = atob(img)
-link = atob(link)
+img = atob(img);
+link = atob(link);
 
-console.log(theme)
+console.log(theme);
 
 if (theme != "black") {
-    $("body").addClass("white");
-    console.log("배경 하양")
+  $("body").addClass("white");
+  console.log("배경 하양");
 } else {
-    $("body").addClass("black");
-    console.log("배경 검정")
+  $("body").addClass("black");
+  console.log("배경 검정");
 }
 if (img == "" || link == "") {
-    console.log("틀림")
+  console.log("틀림");
 } else {
-    $("#link").attr("href", link);
-    $("#img").attr("src", img);
+  $("#link").attr("href", link);
+  $("#img").attr("src", img);
 }
--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 function copylink() {
-    var copyText = document.getElementById("output");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-    document.execCommand("copy");
-    }
-
-function golink() {
-    var imglink = btoa($("#img_link").val());
-    var hyperlink = btoa($("#hyper_link").val());
-    var theme = $("#theme").val()
-
-    if (theme == "다크모드") {
-        theme = "black";
-    } else {
-        theme = "white";
-    }
-
-    var output = `https://hyunsdev.github.io/notion-tools-img-link/block/?theme=${theme}&img=${imglink}&link=${hyperlink}`
-
-    $("#output").val(output)
-    copylink()
-    alert("링크가 복사되었습니다.")
+  var copyText = document.getElementById("output");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
 }
-    
+function golink() {
+  var imglink = btoa($("#img_link").val());
+  var hyperlink = btoa($("#hyper_link").val());
+  var theme = $("#theme").val();
+  if (theme == "다크모드") {
+    theme = "black";
+  } else {
+    theme = "white";
+  }
+  var output = `https://hyunsdev.github.io/notion-tools-img-link/block/?theme=${theme}&img=${imglink}&link=${hyperlink}`;
+  $("#output").val(output);
+  copylink();
+  alert("링크가 복사되었습니다.");
+}
